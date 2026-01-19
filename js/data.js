bcd18ffe0d9312dbbf0aa1be9d31029edb06ae28
a34155-ipca <a34155@alunos.ipca.pt>
2026-01-19 09:57:09 +0000

    Imagens ajustadas

--- js/data.js
+++ js/data.js
@@ -4,21 +4,21 @@ const hotelData = {
             id: 1, 
             tipo: 'Ocean Mirror Suite', 
             preco: 1250, 
-            img: 'https://images.unsplash.com/photo-1578683010236-d716f9a3f461?w=800',
+            img: 'imagens\\Ocean Mirror Suite.png',
             desc: 'Vista panorâmica sobre o Atlântico com jacuzzi privativo.' 
         },
-        { 
+        {   
             id: 2, 
             tipo: 'Forest Sanctuary', 
             preco: 890, 
-            img: 'https://images.unsplash.com/photo-1445019980597-93fa8acb246c?w=800',
+            img: 'imagens\\forest sanctuary suite.jpg',
             desc: 'Um retiro de paz imerso na floresta nativa.' 
         },
         { 
             id: 3, 
             tipo: 'Imperial Loft VIP', 
             preco: 2100, 
-            img: 'https://images.unsplash.com/photo-1590490360182-c33d57733427?w=800',
+            img: 'imagens\\Imperial Loft VIP.jpg',
             desc: 'Sofisticação em dois andares com serviço de mordomo 24h.' 
         }
     ],
